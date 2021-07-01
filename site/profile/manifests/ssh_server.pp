a48281812f6eb5ac563c72e0f7dfeb71698e6948
Martin Breen <30955225+martinjb15@users.noreply.github.com>
2021-07-01 23:30:12 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCaJY9fcN/TY464oYK/+ivNorjRXm1rrYxhlKkqhSQzBRlH/C3wQjUaqPUzGvTdylLOG4vixWHslM0XVNmX3H38cG4iDrdDm9siT25b1bCuMALpJXYhJmoC2v4vx3di9lrNAKKb15e3QH49X75wFNDypGRu+7EzrqDHyMN+awMx1AHcKoXa11uFpZcoDPpGWvaeXSG0NEvsnvChzec4xndza0f6JoXgrLt91cAgRLy7qFRaiHhk18z4RVGV+1tYfwfaGKd8cFVzxiqJtFGeVPm4bQk/Hns8+3HJnDp4PKG99AUPgWuqVe0e/xk+9KuBu+XSjJnfHH1B+Yxh8aKazGwH',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCaJY9fcN/TY464oYK/+ivNorjRXm1rrYxhlKkqhSQzBRlH/C3wQjUaqPUzGvTdylLOG4vixWHslM0XVNmX3H38cG4iDrdDm9siT25b1bCuMALpJXYhJmoC2v4vx3di9lrNAKKb15e3QH49X75wFNDypGRu+7EzrqDHyMN+awMx1AHcKoXa11uFpZcoDPpGWvaeXSG0NEvsnvChzec4xndza0f6JoXgrLt91cAgRLy7qFRaiHhk18z4RVGV+1tYfwfaGKd8cFVzxiqJtFGeVPm4bQk/Hns8+3HJnDp4PKG99AUPgWuqVe0e/xk+9KuBu+XSjJnfHH1B+Yxh8aKazGwH',
 	}  
 }
